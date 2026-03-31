c504c3855595b09a2937192b9b1366d9be5875e0
SUDARK <2963502563@qq.com>
2026-03-31 12:47:14 +0800

    优化LocationUtil获取最高固体方块逻辑 | optimize LocationUtil get highest solid block logic

--- src/main/java/sudark2/Sudark/rentLandPro/Util/LocationUtil.java
+++ src/main/java/sudark2/Sudark/rentLandPro/Util/LocationUtil.java
@@ -7,10 +7,11 @@ public class LocationUtil {
 
     public static int getHighestSolidY(World w, int worldX, int worldZ) {
         int referY = w.getHighestBlockYAt(worldX, worldZ);
+        int minY = w.getMinHeight();
 
-        if (referY < -60) return referY;
+        if (referY <= minY) return minY;
 
-        while (!w.getBlockAt(worldX, referY, worldZ).getType().isOccluding())
+        while (referY > minY && !w.getBlockAt(worldX, referY, worldZ).getType().isOccluding())
             referY--;
 
         return referY;
